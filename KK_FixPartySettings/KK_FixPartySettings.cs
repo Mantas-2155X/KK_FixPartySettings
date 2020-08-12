779e4f64a2d2b7fc9ed6789df14a093d3fb27926
Mantas-2155X <syenergyy@gmail.com>
2020-08-12 22:31:03 +0300

    initial

---
+++ KK_FixPartySettings/KK_FixPartySettings.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace KK_FixPartySettings
+{
+    [BepInProcess("Koikatsu Party")]
+    [BepInPlugin(nameof(KK_FixPartySettings), nameof(KK_FixPartySettings), VERSION)]
+    public class KK_FixPartySettings : BaseUnityPlugin
+    {
+        public const string VERSION = "1.0.0";
+
+        private void Awake() => Harmony.CreateAndPatchAll(typeof(KK_FixPartySettings), nameof(KK_FixPartySettings));
+
+        [HarmonyPrefix, HarmonyPatch(typeof(ConfigScene), "Start")]
+        public static void ConfigScene_Start_ChangeDisplay(ConfigScene __instance, ConfigScene.ShortCutGroup[] ___shortCuts)
+        {
+            var arr = Traverse.Create(___shortCuts[6]).Field("_trans").GetValue<RectTransform[]>();
+            arr[0] = __instance.transform.Find("Canvas/imgWindow/ScrollView/Content/Node Addtional_20").GetComponent<RectTransform>();
+        }
+    }
+}
---
+++ KK_FixPartySettings/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KK_FixPartySettings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
+[assembly: AssemblyProduct("KK_FixPartySettings")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("482F66BC-A448-45E9-A4C2-7E72F00C3025")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KK_FixPartySettings.KK_FixPartySettings.VERSION)]
+[assembly: AssemblyFileVersion(KK_FixPartySettings.KK_FixPartySettings.VERSION)]
